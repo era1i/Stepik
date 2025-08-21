e8d896a8ba10e600f0b1c9ddbfe19d0184222a42
erali <e.eraliev4elreg@gmail.com>
2025-08-21 23:08:42 +0300

    first scratch

--- python_advanced/4.6 Matrix. Part 3/Filling_with_diagonals.py
+++ python_advanced/4.6 Matrix. Part 3/Filling_with_diagonals.py
@@ -1,1 +1,22 @@
-Не удается инициализировать устройство PRN
+def gen_matrix(n, m):
+    return [[0 for _ in range(m)] for _ in range(n)]
+
+def fill_diag(matrix):
+    num = 1
+    n = len(matrix) #высота мартицы
+    m = len(matrix[0]) #ширина матрицы
+    for k in range(n + m - 1):
+        for i in range(n):
+            for j in range(m):
+                if n + m == k:
+                    matrix[i][j] = num
+                    num += 1
+                    break
+    return matrix
+
+def print_matrix(matrix):
+    print('\n'.join([' '.join([str(j).ljust(3) for j in i]) for i in matrix]))
+
+n, m = [int(i) for i in input().split()]
+matrix = fill_diag(gen_matrix(n, m))
+print_matrix(matrix)
