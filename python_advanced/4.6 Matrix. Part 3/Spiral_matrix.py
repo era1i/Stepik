c240d5d7732a37050a47e787548fce45a23fff01
erali <e.eraliev4elreg@gmail.com>
2025-08-22 03:06:11 +0300

    updated code

--- python_advanced/4.6 Matrix. Part 3/Spiral_matrix.py
+++ python_advanced/4.6 Matrix. Part 3/Spiral_matrix.py
@@ -8,29 +8,30 @@ def gen_spiral_matrix (n, m):
         d = 1
     for k in range(1, n * m + 1):
         if d == 0:
-            matrix[pos_x][pos_y] = k
+            matrix[pos_y][pos_x] = k
             pos_x += 1
             if pos_x == right_border:
                 d = 1
                 upper_limit += 1
         elif d == 1:
-            matrix[pos_x][pos_y] = k
+            matrix[pos_y][pos_x] = k
             pos_y += 1
             if pos_y == lower_limit:
                 d = 2
                 right_border -= 1
         elif d == 2:
-            matrix[pos_x][pos_y] = k
+            matrix[pos_y][pos_x] = k
             pos_x -= 1
             if pos_x == left_border:
                 d = 3
                 lower_limit -= 1
         else:
-            matrix[pos_x][pos_y] = k
+            matrix[pos_y][pos_x] = k
             pos_y -= 1
             if pos_y == upper_limit:
                 d = 0
                 left_border += 1
+    return matrix
 
 def print_matrix(matrix):
     print('\n'.join([' '.join([str(j).ljust(3) for j in i]) for i in matrix]))
